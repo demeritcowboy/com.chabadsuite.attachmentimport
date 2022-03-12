d1e52024db033ee7e7715d8714f8782ddec1dab0
demeritcowboy <demeritcowboy@hotmail.com>
2022-03-12 14:23:05 -0500

    only compare 15 chars of salesforce id

--- Civi/Api4/Action/AttachmentImport/Import.php
+++ Civi/Api4/Action/AttachmentImport/Import.php
@@ -30,16 +30,26 @@ class Import extends \Civi\Api4\Generic\AbstractAction {
     $newFolder = rtrim(\CRM_Core_Config::singleton()->customFileUploadDir, "/\\") . '/';
     $fp = fopen($this->importfile, 'r');
     $header = fgetcsv($fp);
-    // for now assume header looks like Attachment ID,Body,File Name,Parent ID,Parent.ID
+    // For now assume header looks like Attachment ID,Body,File Name,Parent ID,Parent.ID
     while (($row = fgetcsv($fp)) !== FALSE) {
       // Locate the contact based on the custom field.
-      $contact = \Civi\Api4\Contact::get()
+      // Note that Parent ID has 18 chars but the contact IDs are 15 chars, but
+      // the last 3 chars can be ignored. Also there is contact id and account
+      // id - we try contact first then account.
+      $salesforce_contact_id = substr($row[3], 0, 15);
+      $contact = \Civi\Api4\Contact::get(FALSE)
         ->addSelect('id')
-        ->addWhere('Legacy_ID.Salesforce_Account_ID', '=', $row[3])
+        ->addWhere('Legacy_ID.Salesforce_Contact_ID', '=', $salesforce_contact_id)
         ->execute()->first();
       if (empty($contact['id'])) {
-        \Civi::log()->error("Unable to find contact matching {$row[3]}.");
-        continue;
+        $contact = \Civi\Api4\Contact::get(FALSE)
+          ->addSelect('id')
+          ->addWhere('Legacy_ID.Salesforce_Account_ID', '=', $salesforce_contact_id)
+          ->execute()->first();
+        if (empty($contact['id'])) {
+          \Civi::log()->error("Unable to find contact matching {$salesforce_contact_id}.");
+          continue;
+        }
       }
 
       // It's usually prefixed with the attachment ID, but sometimes not.
@@ -64,7 +74,7 @@ class Import extends \Civi\Api4\Generic\AbstractAction {
       $mime = mime_content_type($newFilename) ?: 'application/octet-stream';
 
       // Create file record
-      $fileResult = \Civi\Api4\File::create()
+      $fileResult = \Civi\Api4\File::create(FALSE)
         ->addValue('mime_type', $mime)
         ->addValue('uri', basename($newFilename))
         ->addValue('description', $row[2])
@@ -76,6 +86,7 @@ class Import extends \Civi\Api4\Generic\AbstractAction {
 
       // Create custom field record
       \Civi\Api4\CustomValue::create('CS_Attachments')
+        ->setCheckPermissions(FALSE)
         ->addValue('cs_attachment_file', $fileResult['id'])
         ->addValue('cs_attachment_description', E::ts('Imported'))
         ->addValue('entity_id', $contact['id'])
